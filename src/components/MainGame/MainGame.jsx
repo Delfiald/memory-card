afd5cee713f0154760620fd9ab8cb0be56e633ea
M.Aldi Gunawan <aldigunawan1808@gmail.com>
2024-12-19 10:55:20 +0700

    Implement Card Component and Game Logic
    - Add card component to display Pokémon name, type, and image
    - Implement hover effect to tilt the card based on cursor position
    - Add game logic for scoring and tracking clicked cards
    - Use useEffect to check if the game is finished
    - Refactor pokemonList state to include an isClicked property in each Pokémon object
    - Remove totalScore state and derive score from the length of clicked Pokémon
    - Add gameState state to track whether the game is active or finished

--- src/components/MainGame/MainGame.jsx
+++ src/components/MainGame/MainGame.jsx
@@ -1,11 +1,50 @@
 import "./mainGame.css";
 import Card from "../Card/Card";
 
-function MainGame(currentScore, setCurrentScore, setBestScore, totalScore) {
+function MainGame({
+ gameState,
+ setGameState,
+ currentScore,
+ setCurrentScore,
+ setBestScore,
+ pokemonList,
+ setPokemonList,
+ setSavedCard,
+}) {
+ const handleClickedCard = (id) => {
+  if (!gameState) return;
+
+  if (!pokemonList.some((pokemon) => pokemon.id === id && pokemon.isClicked)) {
+   setSavedCard((prevState) => [...prevState, id]);
+   setPokemonList((prevPokemon) =>
+    prevPokemon.map((pokemon) =>
+     pokemon.id === id
+      ? {
+         ...pokemon,
+         isClicked: true,
+        }
+      : pokemon
+    )
+   );
+   setCurrentScore((prevScore) => prevScore + 1);
+   setBestScore((prevBestScore) => Math.max(prevBestScore, currentScore + 1));
+  } else {
+   setGameState(false);
+   console.log("Game Over");
+  }
+ };
+
  return (
   <main>
-   <section className="card-wrapper">
-    <Card />
+   <section className="cards-list-wrapper">
+    {pokemonList.map((pokemon) => (
+     <Card
+      key={pokemon.id}
+      setCurrentScore={setCurrentScore}
+      pokemon={pokemon}
+      handleClickedCard={handleClickedCard}
+     />
+    ))}
    </section>
   </main>
  );
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,25 +1,54 @@
 import Header from "../components/Header/Header";
 import MainGame from "../components/MainGame/MainGame";
 import Footer from "../components/Footer/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Game() {
+ const [gameState, setGameState] = useState(true);
  const [currentScore, setCurrentScore] = useState(0);
  const [bestScore, setBestScore] = useState(0);
- const [totalScore, setTotalScore] = useState(5);
+ const [savedCard, setSavedCard] = useState([]);
+
+ const [pokemonList, setPokemonList] = useState([
+  {
+   id: 1,
+   pokemonImage: "",
+   pokemonName: "Bulbasaur",
+   isClicked: false,
+  },
+  {
+   id: 2,
+   pokemonImage: "",
+   pokemonName: "Charmander",
+   isClicked: false,
+  },
+ ]);
+
+ useEffect(() => {
+  const isFinished = pokemonList.every((pokemon) => pokemon.isClicked);
+
+  if (isFinished) {
+   setGameState(false);
+   console.log("Game Finished");
+  }
+ }, [pokemonList]);
 
  return (
   <>
    <Header
     currentScore={currentScore}
     bestScore={bestScore}
-    totalScore={totalScore}
+    pokemonList={pokemonList.length}
    />
    <MainGame
-    currentScore={setCurrentScore}
+    gameState={gameState}
+    setGameState={setGameState}
+    currentScore={currentScore}
     setCurrentScore={setCurrentScore}
     setBestScore={setBestScore}
-    totalScore={totalScore}
+    pokemonList={pokemonList}
+    setPokemonList={setPokemonList}
+    setSavedCard={setSavedCard}
    />
    <Footer />
   </>
